c2998264a8ce59d35fa35d424497725b4a16a5fa
Colin Landrum <colinlandrum@gmail.com>
2016-04-23 12:08:34 -0700

    added test to fail

--- testhovercraft.py
+++ testhovercraft.py
@@ -4,6 +4,8 @@ import hovercraft
 class TestHoverCraft(unittest.TestCase):
     def test_fullof(self):
         self.assertEqual(hovercraft.fullof(),"eels")
+    def test_fullerof(self):
+        self.assertEqual(hovercraft.fullof(),"fjord")
 
 if __name__ == '__main__':
     unittest.main()
